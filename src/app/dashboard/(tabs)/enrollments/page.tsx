b787524016748270a5b2f2fdb9d497b2fec25058
aimal <aimalshah62@gmail.com>
2025-05-30 11:47:21 +0500

    add enrollments table

--- src/app/dashboard/(tabs)/enrollments/page.tsx
+++ src/app/dashboard/(tabs)/enrollments/page.tsx
@@ -1,5 +1,12 @@
-export default function Page(){
+import { getEnrollmentsWithDetails } from "@/app/db/queries/enrollments/get";
+import { enrllmentColumns } from "@/components/column";
+import DataTable from "@/components/data-table";
+
+export default async function Page(){
+    const data = await getEnrollmentsWithDetails();
     return(
-        <div>Enrollments Page</div>
+        <div className="p-8"> 
+            <DataTable data={data} columns={enrllmentColumns} searchBy="studentFirstName"/>
+        </div>
     )
 }
